4a2cd31d84a9e0c6391de1981f6d98837f703d0d
Rudimix <zeeerobro@gmail.com>
2024-03-15 17:33:10 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using FigureClassLibrary;
+
+namespace InternDataEngeneerTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Figure figure = new Triangle(1, 2, 3);
+            figure = new Circle(1);
+            Console.WriteLine(figure.GetArea());
+        }
+    }
+}
